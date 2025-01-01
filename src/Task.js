55ec3d2998fdf9b66584cec417f4ada7efc58122
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-01-01 17:34:08 +1100

    Add default values for Task constructor

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,11 @@
 export default class Task {
-  constructor(title, desc, dueDate, priority, isCompleted) {
+  constructor(
+    title,
+    desc = null,
+    dueDate = null,
+    priority = null,
+    isCompleted = false
+  ) {
     this.title = title;
     this.desc = desc;
     this.dueDate = dueDate;
